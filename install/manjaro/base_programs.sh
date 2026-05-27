c533eaeff0be46b22a13d5005d28de40bb0e3d39
Nyaniko~ <carotte317@gmail.com>
2026-05-27 22:48:06 +0200

    feat(scripts): Add zip/unzip on manjaro install

--- install/manjaro/base_programs.sh
+++ install/manjaro/base_programs.sh
@@ -2,5 +2,5 @@
 
 sudo pacman -Syu yay
 mkdir ~/Scripts
-yay -Syu zsh librewolf discord steam volctl pavucontrol gnome-terminal btop
+yay -Syu zsh librewolf discord steam volctl pavucontrol gnome-terminal btop zip unzip
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
