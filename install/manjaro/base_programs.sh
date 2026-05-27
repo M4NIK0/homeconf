140ca410affd1d43ef52e795e4625c3a0f894341
Nyaniko~ <carotte317@gmail.com>
2026-05-27 23:02:59 +0200

    refactor(scripts): Separate manjaro install script secondary and main

--- install/manjaro/base_programs.sh
+++ install/manjaro/base_programs.sh
@@ -2,5 +2,12 @@
 
 sudo pacman -Syu yay
 mkdir ~/Scripts
-yay -Syu zsh librewolf discord steam volctl pavucontrol gnome-terminal btop zip unzip
+
+# Main utility
+yay -Syu zsh librewolf volctl pavucontrol gnome-terminal btop zip unzip
+
+# Secondary
+yay -Syu discord steam grub-customizer vlc scrot xclip
+
+# OMZ install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
