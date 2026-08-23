4f1ab1118c5814a35b4029e58e75589da137f147
Nyaniko~ <carotte317@gmail.com>
2026-08-23 11:17:28 +0200

    feat(scripts): Add minecraft installs to manjaro install

--- install/manjaro/base_programs.sh
+++ install/manjaro/base_programs.sh
@@ -13,10 +13,18 @@ yay -Syu jetbrains-toolbox docker docker-compose dbeaver && systemctl start dock
 sudo mhwd -a pci nonfree 0300
 
 # Secondary
-yay -Syu discord steam grub-customizer vlc vlc-plugin-ffmpeg scrot xclip bleachbit pinta spotify ttf-ms-win11-auto
+yay -Syu discord steam grub-customizer vlc vlc-plugin-ffmpeg scrot xclip bleachbit pinta spotify ttf-ms-win11-auto telegram
+
+# Games
+yay -Syu prismlauncher-bin bedrock-on-linux-bin
 
 # Fix emojis on Discord
 sudo pacman -S noto-fonts-emoji
 
 # OMZ install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Custom scripts/zsh custom stuff
+mkdir ~/Scripts
+cp ./scripts/* ~/Scripts
+cat ./zsh_aliases >> ~/.zshrc
